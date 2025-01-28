9177b7ca88873d25cef3d36efba3693d547a227c
ADNAN ALI BHUTTO <138211266+adnanbhutto786@users.noreply.github.com>
2025-01-29 02:43:48 +0500

    User Input complete prompt topic

---
+++ Javascript/02_basic/app.js
@@ -0,0 +1,143 @@
+// console.log("Hello World");
+
+
+// let age=25;
+// let price=11.22
+// let gpa=2.1;
+// console.log(`you are  ${age} year old`);
+// console.log(`The price is $${price}`);
+// console.log(`the gpa is ${gpa}`);
+
+
+let fullName="ADNAN ALI BHUTTO";
+let age=25;
+let isStudent =true;
+
+document.getElementById("p1").innerText="Your full Name is "+fullName;
+document.getElementById("p2").innerText="You are age is "+age;
+document.getElementById("p3").innerText="Enrolled: "+isStudent;
+
+
+console.log(2**5); ///output 32 the squar value
+
+let  value=3;
+let negValue=-value;
+console.log(negValue);
+
+let str1="Ahmed";
+let str2=" Raza";
+let str3=str1+str2;
+console.log(str3);
+
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter+2);
+
+
+
+
+//increment and decrement
+
+///postfix
+
+// let y , x=6;
+// y=x++;
+// console.log(y);
+// console.log(x);  /// y value=6 and x value=7 and
+
+// let y , x=6;
+// y=x--;
+// console.log(y);
+// console.log(x);   // y=6 and x=6
+
+
+//Now prefixx
+
+
+// let y , x=6;
+// y=++x;
+// console.log(y);
+// console.log(x);  /// both are same 7 //
+
+// let y , x=6;
+// y=--x;
+// console.log(y);
+// console.log(x);   //both are same;
+
+
+
+let y, x=5 , a=4 , b=6;
+y = --x + a++ + ++b;
+console.log(y);
+
+
+
+let students=40;
+
+// students=students+1;
+// console.log(students);
+
+// students=students-1;
+// console.log(students);
+
+// students=students*2;
+// console.log(students);
+
+// students=students **2;
+// console.log(students);
+
+
+// students +=1;
+// console.log(students);
+
+// students -=2;
+// console.log(students);
+
+// students **=2;
+// console.log(students);
+
+
+// students /=2;
+// console.log(students);
+
+
+
+let userName,fatherName,emailId,Address;
+userName=prompt("What's your username ?",'Adnan');
+fatherName=prompt("What's your father name ?",'Raza Muhmmad');
+emailId=prompt("What's your Email Id ?", 'Adnan2292707@gmail.com');
+Address=prompt("What's your Home Address ?",'Gulshan E Hadded Karachi')
+
+console.log(userName);
+console.log(fatherName);
+console.log(emailId);
+console.log(Address);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
