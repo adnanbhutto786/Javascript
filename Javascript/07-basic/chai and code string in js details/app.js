26be60ce0fa85be0f3c5e1c73dcee9be4bfcfafe
ADNAN ALI BHUTTO <138211266+adnanbhutto786@users.noreply.github.com>
2025-02-01 01:03:17 +0500

    String-js-its function details

---
+++ Javascript/07-basic/chai and code string in js details/app.js
@@ -0,0 +1,49 @@
+const name="Ali";
+const newName="Ahmed"
+// console.log(name + " "+newName); /// bad partice
+
+console.log(`my name is ${name} and my newName is ${newName}`);
+
+
+
+const newGame= new String('Adnan-hc');
+console.log(newGame);
+
+console.log(newGame [0]);
+console.log(newGame. __proto__);
+console.log(newGame.length);
+console.log(newGame.toLowerCase());
+console.log(newGame.toUpperCase());
+console.log(newGame.charAt(2)); /// postion btaya gaa jp n hai 
+console.log(newGame.indexOf('a'));
+
+console.log(newGame.substring(0,4));
+console.log(newGame.slice(0,6));
+
+
+const newString1="    Javed     ";
+console.log(newString1);
+console.log(newString1.trim());  //space katham krdegaa
+console.log(newString1.trimEnd()); //end say space katham karwegaa
+console.log(newString1.trimStart()); //start say space katha karega
+
+
+const url="https://www.youtube.com/watch?v=_KqpeDc47Ro&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=12";
+console.log(url.replace('//', '||'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
